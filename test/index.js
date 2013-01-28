b289dda7a9a69172c37322c41090c258a9007907
liwei <bjmu_lw@163.com>
2013-01-29 02:02:04 +0800

    listen0.0.0.0

--- test/index.js
+++ test/index.js
@@ -3,4 +3,4 @@ var http = require( 'http' );
 http.createServer(function ( req, res ) {
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
   res.end( 'Hello World.\n' );
-}).listen( 3000, '127.0.0.1' );
+}).listen( 3000, '0.0.0.0' );
