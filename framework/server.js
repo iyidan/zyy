4201348405cce8cffccaa71b5da0ca90d95e714c
liwei <bjmu_lw@163.com>
2013-02-10 22:25:55 +0800

    test

--- framework/server.js
+++ framework/server.js
@@ -8,13 +8,35 @@ exports.createServer = function ( port, ip, callback ) {
       throw new Error( __filename + 'createServer params is empty.' );
     }
     http.createServer(function( req, res ){
-      req = Object.create( req, {
-        'ori':req
-      });
-      res = Object.create( res, {
-        'ori':res
-      });
+      req = inherits_req( req );
+      res = inherits_res( res );
       console.log(req, res);
       callback(req, res);
     }).listen(port, ip);
 };
+
+/**
+ * 包装最初的request
+ * @param  {Object} oriReq 
+ * @return {Object}        
+ */
+function inherits_req( oriReq ) {
+  return Object.create( oriReq, {
+    'ori': {
+      value:oriReq
+    }
+  });
+}
+
+/**
+ * 包装最初的response
+ * @param  {Object} oriRes 
+ * @return {Object}        
+ */
+function inherits_res ( oriRes ) {
+  return Object.create( oriRes, {
+    'ori': {
+      value:oriRes
+    }
+  });
+}
