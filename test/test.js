c4eb9d707ecaeb82ba79807d07774df654ea487c
liwei <bjmu_lw@163.com>
2013-02-11 04:33:55 +0800

    test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ http.createServer(function ( req, res ) {
   var startT = (new Date(req.socket._idleStart)).getTime();
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
   var endT   = (new Date()).getTime();
-  res.write( util.inspect( req ) );
+  res.write( req.socket._idleStart );
+  //res.write( util.inspect( req ) );
   res.end( '\nHello World.\n' + ( endT - startT ) );
 }).listen( 3000, '127.0.0.1' );
