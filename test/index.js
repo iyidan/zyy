45a7eb945d25d79833e650148c403a77c8a194d7
liwei <bjmu_lw@163.com>
2013-02-10 23:07:15 +0800

    test

--- test/index.js
+++ test/index.js
@@ -10,5 +10,5 @@ server.createServer(3000, '127.0.0.1', function ( req, res ) {
   console.log(res);
 
   res.ori.writeHead( 200, { 'Content-Type': 'text/plain' } );
-  res.ori.end( util.inspect( req, true, 3 ) );
+  res.ori.end( util.inspect( req.prototype, true, 3 ) );
 });
