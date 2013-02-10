57067dd03b0534490ae2112e991d083a5cd09ad2
liwei <bjmu_lw@163.com>
2013-02-11 04:24:22 +0800

    test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ http.createServer(function ( req, res ) {
   var startT = (new Date()).getTime();
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
   var endT   = (new Date()).getTime();
-  res.send( util.inspect( req ) );
+  res.write( util.inspect( req ) );
   res.end( '\nHello World.\n' + ( endT - startT ) );
 }).listen( 3000, '127.0.0.1' );
