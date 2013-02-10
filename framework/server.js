b22968279215c7ed6c7eb2a6d42315f65718388f
liwei <bjmu_lw@163.com>
2013-02-10 22:14:05 +0800

    test

--- framework/server.js
+++ framework/server.js
@@ -1,4 +1,19 @@
 var http = require( 'http' );
 
-var Request = require( './Request.js' );
-var Response = require( './Response.js' );
+/**
+ * 创建一个http服务器
+ */
+exports.createServer = function ( port, ip, callback ) {
+    if ( !port || !ip ) {
+      throw new Error( __filename + 'createServer params is empty.' );
+    }
+    http.createServer(function( req, res ){
+      req = Object.create( req, {
+        'ori':req
+      });
+      res = Object.create( res, {
+        'ori':res
+      });
+      callback(req, res);
+    }).listen(port, ip);
+};
--- test/index.js
+++ test/index.js
@@ -1,12 +1,11 @@
 var http   = require( 'http' );
 var assert = require( 'assert' );
 
-var Request  = require( '../framework/Request.js' );
-var Response = require( '../framework/Response.js' );
+var server  = require( '../framework/server.js' );
 
-http.createServer(function ( req, res ) {
+server.createServer(3000, '127.0.0.1', function ( req, res ) {
   req = Request( req );
   res = Response( res );
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
   res.end( 'Hello World.\n' );
-}).listen( 3000, '127.0.0.1' );
+});
