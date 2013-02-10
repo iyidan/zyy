af80bd389de82396d001756e34d8e5fb60ddafc6
liwei <bjmu_lw@163.com>
2013-02-10 23:13:49 +0800

    test

--- test/index.js
+++ test/index.js
@@ -9,6 +9,6 @@ server.createServer(3000, '127.0.0.1', function ( req, res ) {
   console.log('####################');
   console.log(res);
 
-  res.ori.writeHead( 200, { 'Content-Type': 'text/plain' } );
-  res.ori.end( util.inspect( req.prototype, true, 3 ) );
+  res.writeHead( 200, { 'Content-Type': 'text/plain' } );
+  res.end( util.inspect( req.__proto__, true, 3 ) );
 });
