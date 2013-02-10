049445ea413524b47f5eaf713a0925ee1122e987
liwei <bjmu_lw@163.com>
2013-02-11 04:41:40 +0800

    test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@ var http = require( 'http' );
 var util = require( 'util' );
 
 http.createServer(function ( req, res ) {
-  var startT = req.socket._idleStart.getTime();
+  
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
+  var startT = req.socket._idleStart.getTime();
   var endT   = (new Date()).getTime();
-  res.write( util.inspect( typeof req.socket._idleStart ) );
   //res.write( util.inspect( req ) );
   res.end( '\nHello World.\n' + ( endT - startT ) );
 }).listen( 3000, '127.0.0.1' );
