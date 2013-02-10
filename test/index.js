4dae0305a324091cdbbdd4e6904fbf11b8cf6f05
liwei <bjmu_lw@163.com>
2013-02-11 03:09:22 +0800

    test init.js

--- test/index.js
+++ test/index.js
@@ -1,14 +1,5 @@
-var http   = require( 'http' );
-var assert = require( 'assert' );
-var util   = require( 'util' );
+var server = require( '../framework/server.js' );
 
-var server  = require( '../framework/server.js' );
-
-server.createServer(3000, '127.0.0.1', function ( req, res ) {
-  console.log(req);
-  console.log('####################');
-  console.log(res);
-
-  res.writeHead( 200, { 'Content-Type': 'text/plain' } );
-  res.end( util.inspect( req.__proto__, true, 3 ) );
+server.createServer(3000, '127.0.0.1', function(req, res){
+  console.log( 'request in ' + req.SERVER.url.href );
 });
