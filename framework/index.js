6137bba97e651bd1edcd3d8228d0d9fa3077b0d0
liwei <bjmu_lw@163.com>
2013-02-11 03:14:13 +0800

    test init.js

---
+++ framework/index.js
@@ -0,0 +1,19 @@
+var http  = require( 'http' );
+var init  = require( './init.js' ).init;
+var core  = require('./core/index.js');
+
+/**
+ * 创建一个http服务器
+ * @param {Number} port 监听端口
+ * @param {String} ip 监听ip地址
+ * @param {Function} callback  请求被初始化后调用的函数
+ * @return {Object} 返回有http.createServer 生成的server实例
+ */
+exports.createServer = function ( port, ip, callback ) {
+    var port = port || 3000;
+    var ip   = ip || '127.0.0.1';
+    return http.createServer(function(oriRequest, oriResponse){
+      var thisRequest = init(oriRequest, oriResponse);
+      callback( thisRequest[0], thisRequest[1] );
+    }).listen(port, ip);
+};
