c2d4475bd4c9a845f1387705dba71c03c711a18c
liwei <bjmu_lw@163.com>
2013-02-11 02:45:15 +0800

    test init.js

--- framework/init.js
+++ framework/init.js
@@ -1,20 +1,17 @@
 /**
+ * http连接建立后集中初始化请求响应对象
+ */
+
+var Request  = require( './Request.js' ).Request;
+var Response = require( './Response.js' ).Response;
+
+/**
  * 包装原始的req对象
  * @param  {Object} oriReq 原始的request
  * @return {Object} 包装后的request
  */
-exports.initReq = function( oriReq ){
-  
-  return {
-    'ori':oriReq,
-
-    'COOKIE':{},
-    'SESSION':{},
-    'HEADERS':{},
-    'POST':{},
-    'GET':{},
-    'REQUEST':{},
-    'FILES':{},
-  };
-
+exports.init = function( oriReq, oriRes ){
+  var req = new Request( oriReq );
+  var res = new Response( oriRes );
+  return [req, res];
 };
