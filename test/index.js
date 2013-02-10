3f2f5970525e70c4d5fa6143ec52a08f9496378b
liwei <bjmu_lw@163.com>
2013-02-10 22:14:37 +0800

    test

--- test/index.js
+++ test/index.js
@@ -4,8 +4,6 @@ var assert = require( 'assert' );
 var server  = require( '../framework/server.js' );
 
 server.createServer(3000, '127.0.0.1', function ( req, res ) {
-  req = Request( req );
-  res = Response( res );
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
   res.end( 'Hello World.\n' );
 });
