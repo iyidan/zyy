cf9da3033e03af2397f538aae81bf53e39cde8fa
liwei <bjmu_lw@163.com>
2013-02-10 21:15:25 +0800

    test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var http   = require( 'http' );
 var assert = require( 'assert' );
+var test = 'test';
 
 http.createServer(function ( req, res ) {
   res.writeHead( 200, { 'Content-Type': 'text/plain' } );
