0d5826c26948c76ffb40ed6a11d411f333452965
liwei <bjmu_lw@163.com>
2013-02-12 20:38:02 +0800

    test

--- test/index.js
+++ test/index.js
@@ -4,8 +4,8 @@ var util   = require( 'util' );
 server.createServer(3000, '127.0.0.1', function( app ){
   console.log( 'request in ' + app.SERVER('url').href );
 
+  app.res.writeHead(200, { 'Contet-type': 'text/plain' });
   app.sub( 'testEvent', function( data ){
-    app.res.writeHead(200, { 'Contet-type': 'text/plain' });
     app.res.write( util.inspect( data ) );
   });
 
