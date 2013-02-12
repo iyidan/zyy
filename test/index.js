de22dbd8a04179613d9019c59a67fef9fbf5fa81
liwei <bjmu_lw@163.com>
2013-02-12 18:00:02 +0800

    test

--- test/index.js
+++ test/index.js
@@ -2,5 +2,6 @@ var server = require( '../framework/index.js' );
 
 server.createServer(3000, '127.0.0.1', function( app ){
   console.log( 'request in ' + app.SERVER('url').href );
+  console.log( module );
   app.res.end( 'Hello World.\n' );
 });
