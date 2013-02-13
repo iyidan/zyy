a766adeba7e1e017732b16b919544867fee81038
liwei <bjmu_lw@163.com>
2013-02-14 07:15:52 +0800

    test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,16 @@ var util   = require( 'util' );
 var server = server.createServer( config , function( app ){
   app.sub( 'testEvent1', 'testEvent2', 'testEvent3', function( dataList ){
     console.log( dataList );
-    app.end( util.inspect( app, true, null ) );
+    app.write( util.inspect( app, true, null ) );
+    app.write('<hr/><hr/>');
+    app.pub('testevents.ready');
+  });
+
+  app.sub( 'Event1', 'Event2', 'Event3', function( dataList ){
+    console.log( dataList );
+    app.write( util.inspect( app, true, null ) );
+    app.write('<hr/><hr/>');
+    app.pub('events.ready');
   });
 
   app.pub('testEvent', 'world.');
@@ -13,4 +22,13 @@ var server = server.createServer( config , function( app ){
   app.pub('testEvent2', 'data2');
   app.pub('testEvent2', 'data2.5');
   app.pub('testEvent3', 'data3');
+
+  
+  app.pub('Event1', 'ata1');
+  app.pub('Event2', 'ata2');
+  app.pub('Event2', 'ata2.5');
+  app.pub('Event3', 'ata3'); 
+  app.sub('testevents.ready', 'events.ready', function(){
+    app.end('###################################');
+  }); 
 });
