3f40f6e324d3340efb88033e2499e4bd5b10938f
liwei <bjmu_lw@163.com>
2013-02-14 06:28:10 +0800

    test

--- test/index.js
+++ test/index.js
@@ -3,13 +3,9 @@ var server = require( config.FW_PATH );
 var util   = require( 'util' );
 
 var server = server.createServer( config , function( app ){
-  console.log( 'request in ' + app.SERVER('url').href );
-
   app.sub( 'testEvent', function( data ){
-    console.log( 'testEvent\n' );
     app.COOKIE( 'test', 'test', 3600, true );
     app.end( util.inspect( app ) );
   });
-
   app.pub('testEvent', 'world.');
 });
