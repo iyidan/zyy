9dbc67f213f9fabd359f9029dc5a84b2129c7fde
liwei <bjmu_lw@163.com>
2013-02-15 01:02:19 +0800

    test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,32 @@
 var config = require( './config' ).config;
 var server = require( config.FW_PATH );
 var util   = require( 'util' );
+var utils  = require(config.FW_PATH + '/core/utils.js' );
 
 var server = server.createServer( config , function( message, app ){
   app.writeHead(200, { 'Content-Type':'text/html' });
+
+  app.write( 'rc4:\n' );
+  app.write( utils.rc4('key12341234', 'test') );
+  app.write('\n');
+  app.write( utils.rc4( 'key12341234' , utils.rc4('key12341234', 'test')) );
+  app.write('\n');
+
+  app.write( 'uid:\n' );
+  app.write( utils.uid() );
+  app.write('\n');
+  app.write( utils.uid() );
+  app.write('\n');
+  app.write( utils.uid() );
+  app.write('\n');
+  app.write( utils.uid() );
+  app.write('\n');
+  app.write( utils.uid() );
+  app.write('\n');
+  app.write('\n');
+  app.write('\n');
+  app.write('\n');
+
   app.sub( 'testEvent1', 'testEvent2', 'testEvent3', function( message, dataList ){
     app.write('sub1:\n');
     app.write( util.inspect( dataList ) );
