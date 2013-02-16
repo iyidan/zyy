4bdd2b7f376170cf5e598402e6f49b322eeb53c5
liwei <bjmu_lw@163.com>
2013-02-16 22:11:40 +0800

    test

--- framework/message/test.js
+++ framework/message/test.js
@@ -46,6 +46,8 @@ test.test('default', function(){
   messager.pub('testMessage', 'pub5');
   messager.pub('testMessage', 'pub6');
 
+  console.log( 'messager: ' messager);
+
   var messager2 = new Message();
   for ( var i = 0; i < 50; i ++ ) {
     messager2.sub('testMessage', function(message, data){
@@ -57,5 +59,67 @@ test.test('default', function(){
   }, true, false);
 
   messager2.pub('testMessage', 'testMessager2');
+
+  var app = {}；
+  new Message( false, 10, app);
+  app.pub('testMessage', 'pub1');
+  app.sub('testMessage', function(message, data){
+    assert.deepEqual('pub2', data);
+  });
+
+  app.pub('testMessage', 'pub2');
+  console.log('app._message:', app._message);
 });
 
+
+test.suite('multi');
+test.test('multiSubMessages', function(){
+  
+  var messager = new Message();
+
+  messager.sub('test1', 'test2', 'test3', function(message, data){
+    assert.deepEqual('pub1', data[0]);
+    assert.deepEqual('pub2', data[1]);
+    assert.deepEqual('pub3', data[2]);
+    console.log('multi sub before ok.');
+  });
+
+  messager.pub('test2', 'pub2');
+  messager.pub('test1', 'pub1');
+  messager.pub('test3', 'pub3');
+
+  messager.sub('test1', 'test2', 'test3', function(message, data){
+    assert.deepEqual('pub1', data[0]);
+    assert.deepEqual('pub2', data[1]);
+    assert.deepEqual('pub3', data[2]);
+    console.log('multi sub after ok.');
+  });
+
+  // subPublished= false
+  messager.sub('test1', 'test2', 'test3', function(message, data){
+    assert.deepEqual('pub11', data[0]);
+    assert.deepEqual('pub22', data[1]);
+    assert.deepEqual('pub33', data[2]);
+    console.log('multi sub before ok.');
+  }, false);
+  messager.pub('test2', 'pub22');
+  messager.pub('test1', 'pub11');
+  messager.pub('test3', 'pub33');
+
+  // isOnce = false
+  messager.sub('test1', 'test2', 'test3', function(message, data){
+    console.log('[4 times]multi sub isOnce=false ok.');
+  }, true, false);
+
+  messager.pub('test2', 'pub222');
+  messager.pub('test1', 'pub111');
+  messager.pub('test3', 'pub333');
+
+  messager.pub('test3', 'pub3333');
+  messager.pub('test2', 'pub2222');
+  messager.pub('test1', 'pub1111');
+
+  messager.pub('test3', 'pub33333');
+  messager.pub('test2', 'pub22222');
+  messager.pub('test1', 'pub11111');
+});
