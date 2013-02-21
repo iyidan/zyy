f4d6b27f7a2b2b85a7d884bf5e9beb12a02adb89
liwei <bjmu_lw@163.com>
2013-02-22 07:58:40 +0800

    var server = Framework.createserver ...

--- test/index.js
+++ test/index.js
@@ -1,11 +1,10 @@
 var config  = require( './config' ).config;
-var server  = require( config.FW_PATH );
+var Framework  = require( config.FW_PATH );
 var util    = require( 'util' );
 var utils   = require(config.FW_PATH + '/core/utils.js' );
 var Message = require(config.FW_PATH + '/message').Message;
 
-server.createServer( config , function( message, app ){
+var server = Framework.createServer( config , function( message, app ){
   app.writeHead(200, { 'Content-Type':'text/html' });
   app.end( util.inspect(app) );
-  }); 
 });
