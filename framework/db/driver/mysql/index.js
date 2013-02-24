a86664f2c382830f426c1b6ebe661005f4161700
liwei <bjmu_lw@163.com>
2013-02-25 00:29:09 +0800

    test

--- framework/db/driver/mysql/index.js
+++ framework/db/driver/mysql/index.js
@@ -14,7 +14,12 @@ module.exports.mysql = function( db, config )
 /* 原型 */
 var pro = module.exports.mysql.prototype;
 
-/* 只实现这个方法 */
+/**
+ * 查询一条sql
+ * @param  {[type]}   sql      [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
 pro.query = function (sql, callback)
 {
   sql = this.checkSql(sql);
@@ -25,6 +30,9 @@ pro.query = function (sql, callback)
       mysqlWr._db.pub('error', err);
       return;
     }
+    // handler disconnect
+    mysqlWr.handleDisconnect(connection);
+    // query
     connection.query(sql, function(err, data){
       callback(err, data);
       connection.end(function(endErr){
@@ -36,6 +44,31 @@ pro.query = function (sql, callback)
   });
 };
 
+/**
+ * 检查sql
+ * @param  {[type]} sql [description]
+ * @return {[type]}     [description]
+ */
 pro.checkSql = function(sql) {
   return sql;
 };
+
+/**
+ * 监听connection error事件
+ * @param  {[type]} connection [description]
+ * @return {[type]}            [description]
+ */
+pro.handleDisconnect = function(connection) {
+  if (connection._bindOnError) return;
+  connection._bindOnError = true;
+  var mysqlWr = this;
+  connection.on('error', function(err) {
+    if (!err.fatal) {
+      return;
+    }
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      mysqlWr._db.pub('error', err);
+      return;
+    }
+  });
+}
