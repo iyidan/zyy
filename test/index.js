7c52b9d4d4132181bdc7ee0aec5bb526447e0f38
liwei <bjmu_lw@163.com>
2013-02-24 20:51:24 +0800

    test

--- test/index.js
+++ test/index.js
@@ -7,8 +7,9 @@ var Message = require(config.FW_PATH + '/message').Message;
 var server = Framework.createServer( config , function( message, app ){
   //app.writeHead(200, { 'Content-Type':'text/html' });
   app.SESSION('testsession', 'testsessionval');
-  app.db.query('SHOW TABLES', function(err, data){
+  /*app.db.query('SHOW TABLES', function(err, data){
     console.log(err, data);
     app.end(util.inspect(data));
-  });
+  });*/
+  app.end(util.inspect(app));
 });
