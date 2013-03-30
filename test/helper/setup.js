91601b679bae5de411fc21874395c1e1499ef5e8
liwei <bjmu_lw@163.com>
2013-03-31 04:31:36 +0800

    test

--- test/helper/setup.js
+++ test/helper/setup.js
@@ -9,9 +9,9 @@ var common = require('./common');
 module.exports.init = function(app) {
   
 
-  console.log(app.routes);
-  console.log(app._SESSION);
-  console.log(app._sessionid);
+  //console.log(app.routes);
+  //console.log(app._SESSION);
+  //console.log(app._sessionid);
 
   // 解析用户信息
   common.remember_me(app, function(err, user_info){
