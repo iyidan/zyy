e8614d6c3ecd050749ea261c0e121bb1c7d1bf63
iyidan <bjmu_lw@163.com>
2013-04-08 10:39:28 +0800

    add node-redis

---
+++ framework/3rd/node_modules/hiredis/build/Release/.deps/Release/obj.target/hiredis/deps/hiredis/net.o.d
@@ -0,0 +1,9 @@
+cmd_Release/obj.target/hiredis/deps/hiredis/net.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' -I/home/www/.node-gyp/0.8.18/src -I/home/www/.node-gyp/0.8.18/deps/uv/include -I/home/www/.node-gyp/0.8.18/deps/v8/include  -Wall -pthread -m32 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-tree-sink  -MMD -MF ./Release/.deps/Release/obj.target/hiredis/deps/hiredis/net.o.d.raw  -c -o Release/obj.target/hiredis/deps/hiredis/net.o ../deps/hiredis/net.c
+Release/obj.target/hiredis/deps/hiredis/net.o: ../deps/hiredis/net.c \
+ ../deps/hiredis/fmacros.h ../deps/hiredis/net.h \
+ ../deps/hiredis/hiredis.h ../deps/hiredis/sds.h
+../deps/hiredis/net.c:
+../deps/hiredis/fmacros.h:
+../deps/hiredis/net.h:
+../deps/hiredis/hiredis.h:
+../deps/hiredis/sds.h:
---
+++ framework/3rd/node_modules/hiredis/build/Release/.deps/Release/obj.target/hiredis/deps/hiredis/sds.o.d
@@ -0,0 +1,5 @@
+cmd_Release/obj.target/hiredis/deps/hiredis/sds.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' -I/home/www/.node-gyp/0.8.18/src -I/home/www/.node-gyp/0.8.18/deps/uv/include -I/home/www/.node-gyp/0.8.18/deps/v8/include  -Wall -pthread -m32 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-tree-sink  -MMD -MF ./Release/.deps/Release/obj.target/hiredis/deps/hiredis/sds.o.d.raw  -c -o Release/obj.target/hiredis/deps/hiredis/sds.o ../deps/hiredis/sds.c
+Release/obj.target/hiredis/deps/hiredis/sds.o: ../deps/hiredis/sds.c \
+ ../deps/hiredis/sds.h
+../deps/hiredis/sds.c:
+../deps/hiredis/sds.h:
